7946411f4ffd9f8e096a5b9d2189abd8390f31ba
hagilaash <hagilaashjoshu@gmail.com>
2023-12-15 11:29:24 +0530

    file 1

--- python_basics/Python_file_1.py
+++ python_basics/Python_file_1.py
@@ -1,7 +1,48 @@
 # TOPIC=1 [ARITHMETIC OPERATORS]
 
 # ADDITION
+a = 100
+b = 200
+c=a+b
+
+print("THE ADDITION OF A AND B IS:", c )
+
+
 # SUBTRACTION
+
+
+d=a-b
+
+print("THE SUBTRACTION OF A AND B IS:", d )
+
 # MULTIPLICATION 
 
-# 
+e= 200
+
+f=e*b
+
+print("THE MULTIPLICATION OF E AND B IS:",f)
+
+# DIVISION
+# 1.FLOOR DIVISION
+# 2.INTEGER DIVISION
+
+
+# 1.FLOOR DIVISION --> gives the value in float 
+
+g=250
+h=g/e
+
+print("THE DIVISION OF G AND E IS",h)
+
+# 2.INTEGER DIVISION --> gves the value in integer
+
+i=g//e
+
+print("THE DIVISION OF G AND E IS",i)
+
+
+# MODULUS
+
+
+# POWER
