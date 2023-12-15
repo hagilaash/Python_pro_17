3f3e14543ab258cbedddde0ea892b5a1d85d09c5
hagilaash <hagilaashjoshu@gmail.com>
2023-12-15 11:35:34 +0530

    commiting arithmetic operators

--- python_basics/Python_file_1.py
+++ python_basics/Python_file_1.py
@@ -4,45 +4,38 @@
 a = 100
 b = 200
 c=a+b
-
 print("THE ADDITION OF A AND B IS:", c )
 
-
 # SUBTRACTION
-
-
-d=a-b
-
-print("THE SUBTRACTION OF A AND B IS:", d )
+d=b-a
+print("THE SUBTRACTION OF B AND A IS:", d )
 
 # MULTIPLICATION 
-
 e= 200
-
 f=e*b
-
 print("THE MULTIPLICATION OF E AND B IS:",f)
 
 # DIVISION
 # 1.FLOOR DIVISION
 # 2.INTEGER DIVISION
 
-
 # 1.FLOOR DIVISION --> gives the value in float 
-
 g=250
 h=g/e
-
 print("THE DIVISION OF G AND E IS",h)
 
 # 2.INTEGER DIVISION --> gves the value in integer
-
 i=g//e
-
 print("THE DIVISION OF G AND E IS",i)
 
 
 # MODULUS
-
+j = 13
+k= 5
+l = j % k
+print("THE MODULUS OF J AND K IS",l)
 
 # POWER
+m= 2
+n =2**3 # 2*2*2
+print("THE POWER OF N IS",n)
